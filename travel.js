7efe164788ba20b6d859ea183d0fe8036abb9eeb
Mariana <marianacarapitosantos@gmail.com>
2018-11-23 11:36:07 +0000

    names are right

--- travel.js
+++ travel.js
@@ -12,8 +12,11 @@ function gen_vis() {
 	    centered,
 	    clicked_point;
 
-	var projection = d3.geoMercator()
-	    .translate([width / 2.2, height / 1.5]);
+	var projection = d3.geoNaturalEarth1()
+                   .center([0, 15]) 
+                   .rotate([-9,0])
+                   .scale([1300/(2*Math.PI)]) 
+                   .translate([450,300]);
 
 	var plane_path = d3.geoPath()
 	        .projection(projection);
@@ -40,85 +43,35 @@ function gen_vis() {
     var topology = values[0];
     var names = values[1];
   
-    //ready(topology, names);
-    //if (error) throw error;
-    //function ready(world, names) {
     var countries1 = topojson.feature(topology, topology.objects.countries).features;
-      countries = countries1.filter(function(d) {
-      return names.some(function(n) {
-        if (d.id == n.id)  return d.name = n.name;
-      })});
+    countries = countries1.filter(function(d) {
+    return names.some(function(n) {
+      if (d.id == n.id)  {
+        return d.name = n.name;
+      }
+    })});
 
     g.selectAll("path")
-      .data(topojson.feature(topology, topology.objects.countries)
-          .features)
+      .data(countries)
       .enter()
       .append("path")
-      .attrs({
-        "d": path,
-        "fill": "white"
-        })
+      .attr("d", path)
       .on("mouseover",function(d,i){
-                debugger;
                 d3.select(this).attr("fill","grey");
-                return tooltip.style("hidden", false).html(d.id);
+                return tooltip.style("hidden", false).html(d.name);
             })
             .on("mousemove",function(d){
                 tooltip.classed("hidden", false)
                        .style("top", (d3.event.pageY) + "px")
                        .style("left", (d3.event.pageX + 10) + "px")
-                       .html(d.id);
+                       .html(d.name);
             })
             .on("mouseout",function(d,i){
                 d3.select(this).attr("fill","white");
                 tooltip.classed("hidden", true);
             });
-      //.atrr("fill", colorCountry)
       ;
   });
 }
-
-
-// function mouseOver(d){
-//   console.log("entrou");
-//   d3.select(this).attr("fill","grey");
-//     //return tooltip.style("hidden", false).html(d.id);
-// }
-// function mouseMove(d){
-//   tooltip.classed("hidden", false)
-//          .style("top", (d3.event.pageY) + "px")
-//          .style("left", (d3.event.pageX + 10) + "px")
-//          .html(d.id);
-//             }
-// function mouseOut(d,i){
-//   d3.select(this).attr("fill","white");
-//   //tooltip.classed("hidden", true);
-// }
-
-
-// function ready(world, names) {
-//   //if (error) throw error;
-//   var countries1 = topojson.feature(world, world.objects.countries).features;
-//     countries = countries1.filter(function(d) {
-//     return names.some(function(n) {
-//       if (d.id == n.id) return d.name = n.name;
-//     })});
-//   }
-
-// color country
-function colorCountry(country) {
-  if (visited_countries.includes(country.id)) {
-        // hack to discolor ehtiopia
-        if (country.id == '-99' & country.geometry.coordinates[0][0][0] != 20.590405904059054){
-            return '#e7d8ad'    
-        } else {
-            return '#ffffff';
-        };
-    } else {
-        return '#e7d8ad';
-    }
-}
-
-
 
 
