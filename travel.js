e4727823fe25f78d19b96afe87e78a14b5c62a0b
madalenassembleia <madalena.assembleia@gmail.com>
2018-11-25 22:07:52 +0000

    update travel

--- travel.js
+++ travel.js
@@ -14,9 +14,9 @@ function gen_vis() {
 	    clicked_point;
 
 	var projection = d3.geoNaturalEarth1()
-                   .center([0, 15]) 
+                   .center([0, 15])
                    .rotate([-9,0])
-                   .scale([1300/(2*Math.PI)]) 
+                   .scale([1300/(2*Math.PI)])
                    .translate([450,300]);
 
 	var plane_path = d3.geoPath()
@@ -43,7 +43,7 @@ function gen_vis() {
   Promise.all(promises).then(function(values) {
     var topology = values[0];
     var names = values[1];
-  
+
     var countries1 = topojson.feature(topology, topology.objects.countries).features;
     countries = countries1.filter(function(d) {
     return names.some(function(n) {
@@ -78,18 +78,76 @@ function gen_vis() {
       if (selected_countries.includes(d.id) == false && selected_countries.length < 5){
         d3.select(this).classed('active', true);
         selected_countries.push(d.id);
-        return 
+        return
       }
       else if(selected_countries.includes(d.id) == true){
         d3.select(this).classed('active', false);
         var index = selected_countries.indexOf(d.id);
         selected_countries.splice(index, 1);
-        return 
+        return
       }
       //debugger;
       //d3.select('.acitve').classed('active', false);
       return
     }
+
+    function gen_graph(data){
+
+      var w = window.innerWidth/3, h = window.innerHeight/3;
+      var padding = 20;
+      //var data=[1,2,3,4,5];
+      //var countries=[{"country":2014}, {"country":2015} ,{"country":2016},{"country":2017},{"country":2018}];
+
+      if (!pinnedCountries){
+        pinnedCountries = true;
+          var yscale = d3.scaleLinear()
+                          .domain([0, 50])
+                          .rangeRound([h-padding, padding]);
+
+          var xscale = d3.scaleBand()
+                          .domain(data.map(function(d){ return d.Country;})) // muito errado
+                          .range([padding, w-padding]);
+
+          var yaxis = d3.axisLeft()
+                        .scale(yscale);
+
+          //var bar_w = Math.floor(w/(data[1].length*2))-1;
+
+          var xaxis = d3.axisBottom()
+                        .scale(xscale)
+                        .ticks(5);
+
+          var svg = d3.select("#rankedgraph").append("svg")
+                      .attr("width", w)
+                      .attr("height", h)
+                      .attr("class", "rankedgraph");
+
+          svg.append("g")
+              .attr("class", "yaxis")
+              .attr("transform","translate(30,0)")
+              .call(yaxis);
+
+          //console.log(h-padding-yscale(d.Hostel));
+
+
+          var bar = svg.append("g").attr("id", "GraphBars")
+               .selectAll("rect")
+                   .data(data)
+               .enter().append("rect")
+                   .attr("width",Math.floor(w/((data.length+3)))-1) //21
+                   .attr("height", function(d){ return h-yscale(parseInt(d.Hostel));})
+                   .attr("x", function(d){ return (padding + xscale(d.Country));})
+                   .attr("y", function(d){ return (-(padding) + yscale(parseInt(d.Hostel)));})
+                   .attr("fill","#7AC5CD");
+
+          svg.append("g")
+                .attr("class", "xaxis")
+                .attr("transform", "translate (10," + (h-padding) + ")")
+                .call(xaxis);
+      }
+
+      else{
+          //ir buscar paises do outro mapa
+      }
+    
 }
-
-
